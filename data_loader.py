231049bda2d02fef906ad6df5859ac7dcb752002
Jordanlllll <150335720+Jordanlllll@users.noreply.github.com>
2024-10-20 20:34:03 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -7,12 +7,12 @@ def load_cifar10_data(batch_size=32, num_workers=2):
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    trainset = torchvision.datasets.CIFAR10(root='/home/jdli/data/cifar10', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=num_workers)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    testset = torchvision.datasets.CIFAR10(root='/home/jdli/data/cifar10', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=num_workers)
